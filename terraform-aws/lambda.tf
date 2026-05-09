648a7bbca119381312dfe8dfa575041e92123845
DanHenrique <danielhenrique.al@outlook.com>
2026-05-09 19:06:31 -0300

    update infrA

--- terraform-aws/lambda.tf
+++ terraform-aws/lambda.tf
@@ -1,10 +1,10 @@
 module "lambda" {
   source = "github.com/SthoreH/shd-terraform-aws-lambda?ref=v1.0.0"
 
-  name          = "${local.app_name}-lbd"
-  description   = "Products service Lambda"
-  handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.13"
+  name          = local.app_name
+  description   = local.app_description
+  handler       = local.handler
+  runtime       = local.runtime
   role          = module.lambda_role.role_arn
   zip_file_path = local.lambda_zip_path
 
--- terraform-aws/locals.tf
+++ terraform-aws/locals.tf
@@ -1,5 +1,8 @@
 locals {
-  app_name = "products-service"
+  app_name = "lbd-products-service"
+  app_description = "Products service Lambda"
+  handler = "lambda_function.lambda_handler"
+  runtime = "python3.13"
 
   # Convention: the CI/CD pipeline packages the Lambda to <repo-root>/dist/lambda.zip
   # before terraform plan/apply. Keeping the path here avoids passing -var from workflows.
